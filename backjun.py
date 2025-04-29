6f1ca2426af6fa535b94cdbc064c076bffdb2b34
ProfileNamed <spawn1215er@gmail.com>
2025-04-29 14:32:19 +0000

    25/04/29 23:32

--- backjun.py
+++ backjun.py
@@ -1,15 +1,18 @@
-n = int(input())
-l = []
-for i in range(n):
-    l.append([*map(int,input().split())])
-dp = [0] * n
-for i in range(n):
-    dp[i] = l[i][0]
-    for j in range(n):
-        if j == 0:
-            dp[i] = max(dp[i], dp[i] + l[i][j], dp[i] + l[i][j + 1])
-        elif j == n - 1:
-            dp[i] = max(dp[i], dp[i] + l[i][j], dp[i] + l[i][j - 1])
-        else:
-            dp[i] = max(dp[i], dp[i] + l[i][j], dp[i] + l[i][j - 1], dp[i] + l[i][j + 1])
-print(max(dp))
+n, m = map(int, input().split())
+l = sorted(map(int, input().split()))  # 입력을 정렬하여 사전 순으로 처리
+arr = [0] * m
+used = set()  # 중복 수열을 추적하기 위한 집합
+
+def nm(i):
+    if i == m:
+        seq = tuple(arr)  # 현재 수열을 튜플로 변환
+        if seq not in used:  # 중복 여부 확인
+            used.add(seq)
+            print(*arr)
+        return
+    for j in range(len(l)):
+        if i == 0 or arr[i - 1] <= l[j]:  # 비내림차순 조건
+            arr[i] = l[j]
+            nm(i + 1)
+
+nm(0)
