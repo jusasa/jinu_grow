fc2450e8c6b4c4b1bf8bdfb482aa3e8127f0bede
ProfileNamed <spawn1215er@gmail.com>
2025-05-01 13:45:26 +0000

    리팩토링:  다익스트라 알고리즘 구현

--- backjun.py
+++ backjun.py
@@ -1,34 +1,26 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
-print = sys.stdout.write
-
 n = int(input())
-a = [*map(int, input().split())]
 m = int(input())
+l = [[] for i in range(n + 1)]
+for i in range(m):
+    x, y, c = map(int, input().split())
+    l[x].append((y, c))
+    
+dist = [float('inf') for i in range(n + 1)]
+visited = [False for i in range(n + 1)]
 
-# Precompute palindrome information using DP
-dp = [[False] * n for _ in range(n)]
-
-# Single character substrings are palindromes
-for i in range(n):
-    dp[i][i] = True
-
-# Two consecutive characters are palindromes if they are equal
-for i in range(n - 1):
-    if a[i] == a[i + 1]:
-        dp[i][i + 1] = True
-
-# Check for longer substrings
-for length in range(3, n + 1):  # length of the substring
-    for i in range(n - length + 1):
-        j = i + length - 1  # endpoint of the substring
-        if a[i] == a[j] and dp[i + 1][j - 1]:
-            dp[i][j] = True
-
-# Answer the queries
-for _ in range(m):
-    x, y = map(int, input().split())
-    if dp[x - 1][y - 1]:
-        print("1\n")
-    else:
-        print("0\n")
+x, y = map(int, input().split())
+dist[x] = 0
+st = []
+heapq.heappush(st, (0, x))
+while st:
+    d, v = heapq.heappop(st)
+    if visited[v]:
+        continue
+    visited[v] = True
+    for i in l[v]:
+        if dist[i[0]] > dist[v] + i[1]:
+            dist[i[0]] = dist[v] + i[1]
+            heapq.heappush(st, (dist[i[0]], i[0]))
+print(dist[y])
