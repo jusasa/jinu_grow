940ad97285ca8134093f231da7b46ffbf57f8c27
ProfileNamed <spawn1215er@gmail.com>
2025-05-01 13:59:20 +0000

    +경로 추적 추가

--- backjun.py
+++ backjun.py
@@ -9,6 +9,7 @@ for i in range(m):
     
 dist = [float('inf') for i in range(n + 1)]
 visited = [False for i in range(n + 1)]
+prev = [-1 for i in range(n + 1)]
 
 x, y = map(int, input().split())
 dist[x] = 0
@@ -22,5 +23,14 @@ while st:
     for i in l[v]:
         if dist[i[0]] > dist[v] + i[1]:
             dist[i[0]] = dist[v] + i[1]
+            prev[i[0]] = v
             heapq.heappush(st, (dist[i[0]], i[0]))
-print(dist[y])
+
+path = []
+current = y
+while current != -1:
+    path.append(current)
+    current = prev[current]
+print(dist[y]) 
+print(len(path))
+print(*path[::-1])    
